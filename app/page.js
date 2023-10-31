8c3d9844e260c11d77aace8ff8f682b4406dca89
katrinkan <96773609+katrinkan@users.noreply.github.com>
2023-10-31 17:16:41 +0100

    updated page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,7 @@
-"use client";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
+  redirect("/rules");
 
-  router.push("/rules");
   return;
 }
